9fbeee42e7640a4c2f2559e390891134ded9c32b
Majorx234 <majorx234@googlemail.com>
2022-11-17 23:18:24 +0100

    Add basic example

---
+++ src/file_watcher/bin/main.rs
@@ -0,0 +1,15 @@
+use tokio;
+use watchman_client::prelude::*;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+  let mut client = Connector::new().connect().await?;
+  let resolved = client
+     .resolve_root(CanonicalPath::canonicalize(".")?)
+     .await?;
+
+  // Basic globs -> names
+  let files = client.glob(&resolved, &["**/*.rs"]).await?;
+  println!("files: {:#?}", files);
+  Ok(())
+}
