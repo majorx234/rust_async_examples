a0232e35e8342aeb1c068a105d26ae91be43d494
MajorX234 <majorx234@googlemail.com>
2026-01-10 18:40:40 +0100

    Add simple server skeleton

---
+++ simple_chat_server/src/main.rs
@@ -0,0 +1,23 @@
+use tokio::{
+    io::{AsyncBufReadExt, AsyncReadExt, AsyncWriteExt, BufReader},
+    net::{TcpListener, TcpStream}
+};
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    let listener: TcpListener = TcpListener::bind("localhost:8080").await.expect("cannot bind  sokcet");
+    let (mut socket, _addr): (TcpStream, SocketAddr) = listener.accept().await.expect("cannot establish incomming connection");
+    let (mut sock_reader, mut sock_writer) = socket.split();
+    let mut buf_reader = BufReader::new(&mut sock_reader);
+    let mut line = String::new();
+
+    loop {
+        let bytes_read: usize = buf_reader.read_line(&mut line).await.expect("cannot read sokc data");
+        if bytes_read == 0 {
+            break;
+        }
+        sock_writer.write_all(line.as_bytes()).await.expect("cannot write on sock data");
+        line.clear();
+    }
+}
