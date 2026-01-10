ca9f357960719410d6b644fd795987d233f847d7
MajorX234 <majorx234@googlemail.com>
2026-01-11 00:13:44 +0100

    Add broadcaster to forward messages

--- simple_chat_server/src/main.rs
+++ simple_chat_server/src/main.rs
@@ -1,26 +1,31 @@
 use tokio::{
     io::{AsyncBufReadExt, AsyncReadExt, AsyncWriteExt, BufReader},
-    net::{TcpListener, TcpStream}
+    net::{TcpListener, TcpStream}, sync::broadcast
 };
 use std::net::SocketAddr;
 
 #[tokio::main]
 async fn main() {
-    let listener: TcpListener = TcpListener::bind("localhost:8080").await.expect("cannot bind  sokcet");
+    let listener: TcpListener = TcpListener::bind("127.0.0.1:8080").await.expect("cannot bind  socket");
+    let (tx, mut rx) = broadcast::channel::<String>(50);
     loop {
         let (mut socket, _addr): (TcpStream, SocketAddr) = listener.accept().await.expect("cannot establish incomming connection");
+        let tx = tx.clone();
+        let mut rx = tx.subscribe();
         tokio::spawn( async move{
             let (mut sock_reader, mut sock_writer) = socket.split();
             let mut buf_reader = BufReader::new(&mut sock_reader);
-            let mut line = String::new();
 
             loop {
-                let bytes_read: usize = buf_reader.read_line(&mut line).await.expect("cannot read sokc data");
+                let mut line = String::new();
+                let bytes_read: usize = buf_reader.read_line(&mut line).await.expect("cannot read sock data");
                 if bytes_read == 0 {
                     break;
                 }
-                sock_writer.write_all(line.as_bytes()).await.expect("cannot write on sock data");
-                line.clear();
+                tx.send(line);
+                let lines = rx.recv().await.expect("");
+                sock_writer.write_all(lines.as_bytes()).await.expect("cannot write on sock data");
+                //line.clear();
             }
         });
     }
