527b83ad7a928f561cacd0b95b6c2376072112a4
MajorX234 <majorx234@googlemail.com>
2026-01-11 03:02:06 +0100

    Add check to prevent sending same msg previously received by user

--- simple_chat_server/src/main.rs
+++ simple_chat_server/src/main.rs
@@ -7,9 +7,9 @@ use std::net::SocketAddr;
 #[tokio::main]
 async fn main() {
     let listener: TcpListener = TcpListener::bind("127.0.0.1:8080").await.expect("cannot bind  socket");
-    let (tx, _rx) = broadcast::channel::<String>(50);
+    let (tx, _rx) = broadcast::channel::<(String, SocketAddr)>(50);
     loop {
-        let (mut socket, _addr): (TcpStream, SocketAddr) = listener.accept().await.expect("cannot establish incomming connection");
+        let (mut socket, addr): (TcpStream, SocketAddr) = listener.accept().await.expect("cannot establish incomming connection");
         let tx = tx.clone();
         let mut rx = tx.subscribe();
         tokio::spawn( async move{
@@ -23,14 +23,16 @@ async fn main() {
                         if result.expect("cannot read sock data") == 0 {
                             break;
                         }
-                        let _ = tx.send(line);
+                        let _ = tx.send((line.clone(), addr));
                     }
                     new_msgs = rx.recv() => {
-                        let new_msgs = new_msgs.expect("error in boradcast channel");
-                        sock_writer.write_all(new_msgs.as_bytes()).await.expect("cannot write to sock data");
-                        //line.clear();
+                        let (new_msgs, addr_new) = new_msgs.expect("error in boradcast channel");
+                        if addr != addr_new {
+                            sock_writer.write_all(new_msgs.as_bytes()).await.expect("cannot write to sock data");
+                        }
                     }
                 }
+                line.clear();
             }
         });
     }
